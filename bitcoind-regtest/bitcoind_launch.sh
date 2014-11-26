ade798c49e465c3b668876f866896b2dd92226ad
Assaf Shomer <assafshomer@gmail.com>
2014-11-26 10:44:02 +0200

    both bitcoind and toshi setup and launch are now scripted, so all I need to do is make them and then run the script. Next to automate that part. Note that I didn't want to mess with the toshi Dockerfile which is more generic, so I have a build step where I replace the toshi Dockerfile with an improved one which includes copying the specific init script we need when the image is built. probably a better way is to commit the generic toshi image and then have a new Dockerfile based on it, I'll do that later

---
+++ bitcoind-regtest/bitcoind_launch.sh
@@ -0,0 +1,11 @@
+echo "this is bitcoind launch"
+echo "exporting env vars"
+export tmp=${TOSHI_PORT/tcp:\/\//}
+export TOSHI_IP=${tmp/:*/}
+echo "launching bitcoind daemon in regtest mode"
+bitcoind -regtest -daemon -printtoconsole
+sleep "5"
+echo "Adding Toshi node at IP:"$TOSHI_IP
+bitcoind -regtest addnode $TOSHI_IP onetry
+echo "Mining 101 blocks"
+bitcoind -regtest setgenerate true 101
---
+++ toshi_launch.sh
@@ -0,0 +1,12 @@
+echo "This is toshi launch"
+echo "Setting up evn vars"
+export DATABASE_URL=postgres://postgres:@$DB_PORT_5432_TCP_ADDR:$DB_PORT_5432_TCP_PORT
+export REDIS_URL=redis://$REDIS_PORT_6379_TCP_ADDR:$REDIS_PORT_6379_TCP_PORT
+export TOSHI_ENV=test
+export TOSHI_NETWORK=regtest
+export NODE_ACCEPT_INCOMING=true
+export NODE_LISTEN_PORT=18444
+echo "Migrating postgres db"
+bundle exec rake db:migrate
+echo "Starting toshi node, go visit localhost:5000"
+foreman start -c web=1,block_worker=1,transaction_worker=1,peer_manager=1
