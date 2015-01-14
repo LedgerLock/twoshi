17de11e0fa6bd790506c972601f4fc16e5924e87
Jonathan Rouach <jon@dach.nyc>
2015-01-14 09:03:01 -0500

    Use bitcoin-0.10.0rc3

--- bitcoind-regtest/bitcoind_launch.sh
+++ bitcoind-regtest/bitcoind_launch.sh
@@ -12,16 +12,16 @@ echo "exporting env vars"
 export tmp=${TOSHI_PORT/tcp:\/\//}
 export TOSHI_IP=${tmp/:*/}
 echo "launching bitcoind daemon in regtest mode"
-bitcoind -regtest -daemon -rpcallowip=* -printtoconsole
+bitcoind -regtest -server -daemon -rpcallowip=0/0 -printtoconsole
 # increase the number of seconds to more than 5 if bitcoind didn't manage to connect to toshi
 sleep "5"
 echo "Adding Toshi node at IP:"$TOSHI_IP
-bitcoind -regtest addnode $TOSHI_IP onetry
-export BNUM="$(bitcoind -regtest getblockcount)"
+bitcoin-cli -regtest addnode $TOSHI_IP onetry
+export BNUM="$(bitcoin-cli -regtest getblockcount)"
 if [ $BNUM -eq 0 ] 
 	then
 		echo "Mining 101 blocks"
-		bitcoind -regtest setgenerate true 101
+		bitcoin-cli -regtest setgenerate true 101
 	else
 		echo "Not mining any more blocks, " $BNUM " blocks already found"
 fi
